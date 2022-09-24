a2985e6ac3b2602e97ddae1e4e183933af1585a3
Dat Tran <datitran@gmail.com>
2022-09-24 19:56:49 +0200

    chore: increase to version 0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "priceloop-api"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 # To install the library, run the following
 #
 # python setup.py install

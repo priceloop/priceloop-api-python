548bee79d53560b9ded85524caa436b7dd542a0f
bayoog <obaydaba96@gmail.com>
2022-10-18 22:11:01 +0200

    feat: option to specify workspace

--- priceloop_api/utils/utils.py
+++ priceloop_api/utils/utils.py
@@ -4,16 +4,27 @@ from io import StringIO
 from priceloop_api import ApiClient
 from priceloop_api.api.default_api import DefaultApi
 
-def to_nocode(df: pd.DataFrame, table_name: str, configuration, mode='delete_and_recreate') -> None:
+
+def to_nocode(
+    df: pd.DataFrame,
+    table_name: str,
+    configuration,
+    mode="delete_and_recreate",
+    workspace_name: str = "",
+) -> None:
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=None)
     with ApiClient(configuration) as api_client:
         api_instance = DefaultApi(api_client)
-        workspaces = api_instance.list_workspaces()
-        workspace = api_instance.get_workspace(workspaces[0])
-        url = api_instance.get_table_upload_csv_url(workspace.name, table_name, mode=mode)
-        requests.put(url, data=csv_buffer.getvalue().encode('utf-8'))
-        print('Upload Successful, please wait a moment for the changes to appear')
+        if workspace_name == "":
+            workspaces = api_instance.list_workspaces()
+            workspace_name = api_instance.get_workspace(workspaces[0]).name
+        url = api_instance.get_table_upload_csv_url(
+            workspace_name, table_name, mode=mode
+        )
+        requests.put(url, data=csv_buffer.getvalue().encode("utf-8"))
+        print("Upload Successful, please wait a moment for the changes to appear")
+
 
 def read_nocode(table_name: str, configuration, limit: int, offset: int):
     csv_buffer = StringIO()
@@ -22,9 +33,13 @@ def read_nocode(table_name: str, configuration, limit: int, offset: int):
         workspaces = api_instance.list_workspaces()
         workspace = api_instance.get_workspace(workspaces[0])
         raw_header = api_instance.get_table(workspace.name, table_name)
-        header = ['index'] + [i['name'] for i in raw_header['columns']]
-        raw_table_data = api_instance.get_table_data(workspace.name, table_name, limit=limit, offset=offset)
-        table_data = pd.DataFrame(raw_table_data['rows'], columns=header).drop(columns='index')
+        header = ["index"] + [i["name"] for i in raw_header["columns"]]
+        raw_table_data = api_instance.get_table_data(
+            workspace.name, table_name, limit=limit, offset=offset
+        )
+        table_data = pd.DataFrame(raw_table_data["rows"], columns=header).drop(
+            columns="index"
+        )
         # To-do: infer type from nocode
         table_data.to_csv(csv_buffer, index=None)
         csv_buffer.seek(0)

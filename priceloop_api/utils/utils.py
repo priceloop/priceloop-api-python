8777a665b49bade253a1ce2ca11f3e31fa6dfa55
Obay Daba <obaydaba96@gmail.com>
2022-11-29 13:53:21 +0100

    Fix build (#11)
    * fix: DefaultApi
    
    * wip
    
    * get table upload csv url
    
    * fix: DefaultApi import
    
    * fix: wrong flag
    
    * update requirements file
    
    * wip

--- priceloop_api/utils/utils.py
+++ priceloop_api/utils/utils.py
@@ -2,29 +2,62 @@ import requests
 import pandas as pd
 from io import StringIO
 from priceloop_api import ApiClient
-from priceloop_api.api.default_api import DefaultApi
+from priceloop_api.apis.tags.default_api import DefaultApi
 
-def to_nocode(df: pd.DataFrame, table_name: str, configuration, mode='delete_and_recreate') -> None:
+
+def to_nocode(
+    df: pd.DataFrame,
+    table_name: str,
+    configuration,
+    mode="delete_and_recreate",
+    workspace_name: str = None,
+) -> None:
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=None)
     with ApiClient(configuration) as api_client:
         api_instance = DefaultApi(api_client)
-        workspaces = api_instance.list_workspaces()
-        workspace = api_instance.get_workspace(workspaces[0])
-        url = api_instance.get_table_upload_csv_url(workspace.name, table_name, mode=mode)
-        requests.put(url, data=csv_buffer.getvalue().encode('utf-8'))
-        print('Upload Successful, please wait a moment for the changes to appear')
+        if workspace is None:
+            workspaces = api_instance.list_workspaces().body
+            workspace = api_instance.get_workspace(
+                path_params={"workspace": workspaces[0]}
+            ).body
+            workspace_name = workspace.name
 
-def read_nocode(table_name: str, configuration, limit: int, offset: int):
+        url = api_instance.get_table_upload_csv_url(
+            path_params={"workspace": workspace_name, "table": table_name},
+            query_params={"mode": mode},
+        ).body
+        requests.put(url, data=csv_buffer.getvalue().encode("utf-8"))
+        print("Upload Successful, please wait a moment for the changes to appear")
+
+
+def read_nocode(
+    table_name: str, configuration, limit: int, offset: int, workspace_name: str = None
+):
     csv_buffer = StringIO()
     with ApiClient(configuration) as api_client:
         api_instance = DefaultApi(api_client)
-        workspaces = api_instance.list_workspaces()
-        workspace = api_instance.get_workspace(workspaces[0])
-        raw_header = api_instance.get_table(workspace.name, table_name)
-        header = ['index'] + [i['name'] for i in raw_header['columns']]
-        raw_table_data = api_instance.get_table_data(workspace.name, table_name, limit=limit, offset=offset)
-        table_data = pd.DataFrame(raw_table_data['rows'], columns=header).drop(columns='index')
+        if workspace is None:
+            workspaces = api_instance.list_workspaces().body
+            workspace = api_instance.get_workspace(
+                path_params={"workspace": workspaces[0]}
+            ).body
+            workspace_name = workspace.name
+
+        raw_header = api_instance.get_table(
+            path_params={"workspace": workspace_name, "table": table_name}
+        ).body
+        header = ["index"] + [i["name"] for i in raw_header["columns"]]
+        raw_table_data = api_instance.get_table_data(
+            query_params={"limit": limit, "offset": offset},
+            path_params={
+                "workspace": workspace_name,
+                "table": table_name,
+            },
+        ).body
+        table_data = pd.DataFrame(raw_table_data["rows"], columns=header).drop(
+            columns="index"
+        )
         # To-do: infer type from nocode
         table_data.to_csv(csv_buffer, index=None)
         csv_buffer.seek(0)

b0efcf5e258a9790df302e1c9e06dca9ade3756c
GRBurst <GRBurst@protonmail.com>
2022-12-05 13:36:02 +0100

    feat: allow overwriting of endpoints (for local/docker dev)
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- priceloop_api/utils/default_configuration.py
+++ priceloop_api/utils/default_configuration.py
@@ -8,20 +8,45 @@ default_host_name = 'alpha.priceloop.ai'
 class DefaultConfiguration(priceloop_api.Configuration):
 
     @staticmethod
-    def with_access_token(access_token: str, host: str = default_host_name):
-        return DefaultConfiguration(access_token_func=lambda: access_token, host=host)
+    def with_access_token(
+        access_token: str,
+        host: str = default_host_name,
+        api_endpoint: str = None,
+    ):
+        return DefaultConfiguration( access_token_func = lambda: access_token, host = host, api_endpoint = api_endpoint)
 
     @staticmethod
-    def with_user_credentials(username: str, password: str, host: str = default_host_name):
-        auth_state = AuthState(username=username, password=password, host=host)
-        return DefaultConfiguration(access_token_func=auth_state.access_token, host=host)
+    def with_user_credentials(
+        username: str,
+        password: str,
+        host: str = default_host_name,
+        host_endpoint: str = None,
+        api_endpoint: str = None,
+        auth_endpoint: str = None,
+    ):
+        auth_state = AuthState( username = username, password = password, host = host, host_endpoint = host_endpoint, auth_endpoint = auth_endpoint)
+        return DefaultConfiguration( access_token_func = auth_state.access_token, host = host, api_endpoint = api_endpoint)
 
     @staticmethod
-    def anon(host: str = default_host_name):
-        return DefaultConfiguration(host = host)
+    def anon(
+        host: str = default_host_name,
+        api_endpoint: str = None,
+    ):
+        return DefaultConfiguration(host = host, api_endpoint = api_endpoint)
 
-    def __init__(self, access_token_func: Callable[[], str] = None, host: str = default_host_name):
-        super().__init__(host=f'https://api.{host}')
+    def __init__(
+        self, access_token_func: Callable[[], str] = None,
+        host: str = default_host_name,
+        api_endpoint: str = None,
+    ):
+        endpoint = None
+
+        if api_endpoint is None:
+            endpoint = f'https://api.{host}'
+        else:
+            endpoint = api_endpoint
+
+        super().__init__(host = endpoint)
         self._access_token_func = access_token_func
 
     def auth_settings(self):
--- priceloop_api/utils/utils.py
+++ priceloop_api/utils/utils.py
@@ -9,25 +9,25 @@ def to_nocode(
     df: pd.DataFrame,
     table_name: str,
     configuration,
-    mode="delete_and_recreate",
+    mode = "delete_and_recreate",
     workspace_name: str = None,
 ) -> None:
     csv_buffer = StringIO()
-    df.to_csv(csv_buffer, index=None)
+    df.to_csv(csv_buffer, index = None)
     with ApiClient(configuration) as api_client:
         api_instance = DefaultApi(api_client)
         if workspace_name is None:
             workspaces = api_instance.list_workspaces().body
             workspace = api_instance.get_workspace(
-                path_params={"workspace": workspaces[0]}
+                path_params = {"workspace": workspaces[0]}
             ).body
             workspace_name = workspace.name
 
         url = api_instance.get_table_upload_csv_url(
-            path_params={"workspace": workspace_name, "table": table_name},
-            query_params={"mode": mode},
+            path_params = {"workspace": workspace_name, "table": table_name},
+            query_params = {"mode": mode},
         ).body
-        requests.put(url.putUrl, data=csv_buffer.getvalue().encode("utf-8"))
+        requests.put(url.putUrl, data = csv_buffer.getvalue().encode("utf-8"))
         print("Upload Successful, please wait a moment for the changes to appear")
 
 
@@ -40,26 +40,26 @@ def read_nocode(
         if workspace_name is None:
             workspaces = api_instance.list_workspaces().body
             workspace = api_instance.get_workspace(
-                path_params={"workspace": workspaces[0]}
+                path_params = {"workspace": workspaces[0]}
             ).body
             workspace_name = workspace.name
 
         raw_header = api_instance.get_table(
-            path_params={"workspace": workspace_name, "table": table_name}
+            path_params = {"workspace": workspace_name, "table": table_name}
         ).body
         header = ["index"] + [i["name"] for i in raw_header["columns"]]
         raw_table_data = api_instance.get_table_data(
-            query_params={"limit": limit, "offset": offset},
-            path_params={
+            query_params = {"limit": limit, "offset": offset},
+            path_params = {
                 "workspace": workspace_name,
                 "table": table_name,
             },
         ).body
-        table_data = pd.DataFrame(raw_table_data["rows"], columns=header).drop(
-            columns="index"
+        table_data = pd.DataFrame(raw_table_data["rows"], columns = header).drop(
+            columns = "index"
         )
         # To-do: infer type from nocode
-        table_data.to_csv(csv_buffer, index=None)
+        table_data.to_csv(csv_buffer, index = None)
         csv_buffer.seek(0)
         table_data_type_inferred = pd.read_csv(csv_buffer)
     return table_data_type_inferred

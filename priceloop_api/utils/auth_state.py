830c263fbc536b927d5e4a777ebaaac0f391bf69
GRBurst <GRBurst@protonmail.com>
2022-12-05 13:36:02 +0100

    feat: allow overwriting of endpoints (for local/docker dev)
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- priceloop_api/utils/auth_state.py
+++ priceloop_api/utils/auth_state.py
@@ -5,7 +5,7 @@ import boto3
 import requests
 
 
-@dataclass(frozen=True)
+@dataclass(frozen = True)
 class ApiConfig:
     region: str
     client_id: str
@@ -13,7 +13,7 @@ class ApiConfig:
     auth_url: str
 
 
-@dataclass(frozen=True)
+@dataclass(frozen = True)
 class AuthTokens:
     access_token: str
     expires_in_seconds: int
@@ -29,55 +29,76 @@ class AuthState(object):
 
     def access_token(self):
         if self.nocode_config is None:
-            self.nocode_config = get_config(f'https://{self.host}/app_config.json')
+            endpoint = None
+            if self.host_endpoint is None:
+                endpoint = f'https://{self.host}/app_config.json'
+            else:
+                endpoint = f'{self.host_endpoint}/app_config.json'
+
+            self.nocode_config = get_config(endpoint, self.auth_endpoint)
 
         if self.auth_tokens is None or is_expired(datetime.now(), self.auth_tokens):
-            self.auth_tokens = get_oauth_tokens(self.nocode_config, self.username, self.password)
+            self.auth_tokens = get_oauth_tokens(self.nocode_config, self.username, self.password, self.auth_endpoint)
 
         return self.auth_tokens.access_token
 
-    def __init__(self, username, password, host):
-        self.host = host
+    def __init__(self, username, password, host, host_endpoint, auth_endpoint):
         self.username = username
         self.password = password
+        self.host = host
+        self.host_endpoint = host_endpoint
+        self.auth_endpoint = auth_endpoint
 
 
 def is_expired(now: datetime, tokens: AuthTokens) -> bool:
-    return tokens.expiry_date - timedelta(minutes=1) > now
+    return tokens.expiry_date - timedelta(minutes = 1) > now
 
 
-def get_config(app_config_url: str) -> ApiConfig:
-    cfg = requests.get(app_config_url).json()
+def get_config(app_config_url: str, auth_endpoint = None) -> ApiConfig:
+    response = requests.get(app_config_url)
+    cfg = response.json()
+    endpoint = None
+
+    if auth_endpoint is None:
+        endpoint = cfg['auth']['url']
+    else:
+        endpoint = auth_endpoint
 
     return ApiConfig(
-            region=cfg['region'],
-            client_id=cfg['auth']['clientId'],
-            api_scopes=cfg['auth']['apiScopes'],
-            auth_url=cfg['auth']['url']
+            region = cfg['region'],
+            client_id = cfg['auth']['clientId'],
+            api_scopes = cfg['auth']['apiScopes'],
+            auth_url = endpoint
     )
 
 
-def get_oauth_tokens(config: ApiConfig, user_email: str, user_password: str) -> AuthTokens:
-    cognito_client = boto3.client('cognito-idp', region_name=config.region)
+def get_oauth_tokens(config: ApiConfig, user_email: str, user_password: str, auth_endpoint: str = None) -> AuthTokens:
+    cognito_client = None
+    if auth_endpoint is None:
+        cognito_client = boto3.client('cognito-idp', region_name = config.region)
+    else: 
+        # TODO: this is not working, yet
+        cognito_client = boto3.client('cognito-idp', region_name = config.region, endpoint_url = auth_endpoint)
+
     auth_response = cognito_client.initiate_auth(
-            AuthFlow='USER_PASSWORD_AUTH',
-            AuthParameters={
+            AuthFlow = 'USER_PASSWORD_AUTH',
+            AuthParameters = {
                 'USERNAME': user_email,
                 'PASSWORD': user_password
             },
-            ClientId=config.client_id
+            ClientId = config.client_id,
     )
 
     # calculate expiry date, because cognito only gives the number of seconds
     now = datetime.today()
     expires_in_seconds = int(auth_response['AuthenticationResult']['ExpiresIn'])
-    expiry_date = now + timedelta(seconds=expires_in_seconds)
+    expiry_date = now + timedelta(seconds = expires_in_seconds)
 
     return AuthTokens(
-            access_token=auth_response['AuthenticationResult']['AccessToken'],
-            expires_in_seconds=expires_in_seconds,
-            expiry_date=expiry_date,
-            id_token=auth_response['AuthenticationResult']['IdToken'],
-            refresh_token=auth_response['AuthenticationResult']['RefreshToken'],
-            token_type=auth_response['AuthenticationResult']['TokenType']
+            access_token = auth_response['AuthenticationResult']['AccessToken'],
+            expires_in_seconds = expires_in_seconds,
+            expiry_date = expiry_date,
+            id_token = auth_response['AuthenticationResult']['IdToken'],
+            refresh_token = auth_response['AuthenticationResult']['RefreshToken'],
+            token_type = auth_response['AuthenticationResult']['TokenType']
     )

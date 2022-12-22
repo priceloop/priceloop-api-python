85fef5c0a238198839e1ff21348514d9bfb7e318
GRBurst <GRBurst@protonmail.com>
2022-12-22 12:46:44 +0100

    wip
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- test/test_utils.py
+++ test/test_utils.py
@@ -15,10 +15,10 @@ class UtilsTest(unittest.TestCase):
     def test_write_nocode(self):
         data = {'col1': [1, 2], 'col2': [3, 4]}
         df = pd.DataFrame(data=data)
-        to_nocode(df, os.environ["NOCODE_TEST_TABLE"], self.configuration, mode="new")
+        to_nocode(df, os.environ["NOCODE_TEST_TABLE"], self.configuration, mode="replace_data")
         assert True
 
     def test_read_nocode(self):
-        data = read_nocode(os.environ["NOCODE_TEST_TABLE"],
+        data_read = read_nocode(os.environ["NOCODE_TEST_TABLE"],
                            self.configuration, limit=2, offset=0)
         assert True

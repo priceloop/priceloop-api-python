bc6bc25c43b805c0cd26c9d5948636889b3e5ce7
johannes karoff <johannes.karoff@priceloop.ai>
2023-01-13 17:23:11 +0100

    run generate_api in gh action

--- generate_api.sh
+++ generate_api.sh
@@ -4,12 +4,10 @@ set -Eeuo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-if [[ $(git diff --stat) != '' ]]; then
+if [[ "$(git diff --stat)" != "" ]]; then
   echo 'git repository is dirty, exiting.'
   exit 1
 fi
-
-pipx install openapi-python-client --include-deps
 
 # regenerate the whole python project
 rm -rf priceloop-api-python

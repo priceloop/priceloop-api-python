8b2c9d630384dca18eb9d66085df5ba171d63b3f
johannes karoff <johannes.karoff@priceloop.ai>
2023-01-18 14:48:10 +0100

    fix priceloop client

--- priceloop_api/priceloop/auth/priceloop_client.py
+++ priceloop_api/priceloop/auth/priceloop_client.py
@@ -1,23 +1,28 @@
-from priceloop_api.auth import AuthState
+from .auth import AuthState
 from priceloop_api import Client
-
+from typing import Dict
 import attr
 
 default_host = "alpha.priceloop.ai"
 
 
-@attr.s(auto_attribs=True)
 class PriceloopClient(Client):
     """A Client which has been authenticated for use on secured endpoints"""
 
-    _auth_state: AuthState
+    auth_state: AuthState
 
-    def __init__(auth_state: AuthState):
-        self._auth_state = auth_state
+    def __init__(self, auth_state: AuthState):
+        self.base_url = auth_state.config().base_url
+        self.timeout = 30.0
+        self.cookies = {}
+        self.headers = {}
+        self.verify_ssl = True
+        self.raise_on_unexpected_status = False
+        self.auth_state = auth_state
 
     def get_headers(self) -> Dict[str, str]:
         """Get headers to be used in authenticated endpoints"""
-        auth_header_value = f"Bearer {self._auth_state.access_token()}"
+        auth_header_value = f"Bearer {self.auth_state.access_token()}"
         return {"Authorization": auth_header_value, **self.headers}
 
     @staticmethod

55243589a4a1ba47cba4b95f23c35b911f979b8a
bayoog <obaydaba96@gmail.com>
2023-03-03 16:24:49 +0100

    feat: update `to_nocode`

--- priceloop_api/priceloop/data/data.py
+++ priceloop_api/priceloop/data/data.py
@@ -2,14 +2,14 @@ import requests
 import pandas as pd
 from io import StringIO
 
-from priceloop_api.models.get_table_upload_csv_url_mode import GetTableUploadCsvUrlMode
+from priceloop_api.models.create_csv_import_job_mode import CreateCsvImportJobMode
 from priceloop_api.client import AuthenticatedClient
 from priceloop_api.api.default import (
     list_workspaces,
     get_workspace,
-    get_table_upload_csv_url,
     get_table,
     get_table_data,
+    create_csv_import_job,
 )
 
 d = {"t": True, "f": False}
@@ -19,7 +19,7 @@ def to_nocode(
     df: pd.DataFrame,
     table_name: str,
     client: AuthenticatedClient,
-    mode: GetTableUploadCsvUrlMode = GetTableUploadCsvUrlMode.DELETE_AND_RECREATE,
+    mode: CreateCsvImportJobMode = CreateCsvImportJobMode.DELETE_AND_RECREATE,
     workspace_name: str = None,
 ) -> None:
     csv_buffer = StringIO()
@@ -30,9 +30,9 @@ def to_nocode(
         workspace = get_workspace.sync(workspaces[0], client=client)
         workspace_name = workspace.name
 
-    url = get_table_upload_csv_url.sync(workspace_name, table_name, mode=mode, client=client)
+    import_job = create_csv_import_job.sync(workspace_name, table_name, mode=mode, client=client)
 
-    requests.put(url.put_url, data=csv_buffer.getvalue().encode("utf-8"))
+    requests.put(import_job.put_url, data=csv_buffer.getvalue().encode("utf-8"))
     print("Upload Successful, please wait a moment for the changes to appear")
 
 

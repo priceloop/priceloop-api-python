25fb4773c634c403ed53571bf3c45449c88a654e
GRBurst <GRBurst@protonmail.com>
2023-06-26 11:58:10 +0200

    chore: try to deploy 3.8-pandas1 and 3.11-pandas2 versions
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

--- setup-pandas2.py
+++ setup-pandas2.py
@@ -17,7 +17,7 @@ REQUIRES = [
 
 setup(
     name="priceloop-api",
-    version="0.127.1-dev1",
+    version="0.127.2-dev",
     description="A client library for accessing Priceloop API",
     author="Priceloop",
     author_email="hello@priceloop.ai",

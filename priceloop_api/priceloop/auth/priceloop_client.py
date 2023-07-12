38510d2f441e3afe334daa4db51b6e6d767a0930
DjObYtE <obay.daba@priceloop.ai>
2023-07-12 14:06:53 +0200

    feat: parameterize `raise_on_unexpected_status`

--- priceloop_api/priceloop/auth/priceloop_client.py
+++ priceloop_api/priceloop/auth/priceloop_client.py
@@ -5,18 +5,19 @@ import attr
 
 default_host = "alpha.priceloop.ai"
 
+
 class PriceloopClient(AuthenticatedClient):
     """A Client which has been authenticated for use on secured endpoints"""
 
     auth_state: AuthState
 
-    def __init__(self, auth_state: AuthState):
+    def __init__(self, auth_state: AuthState, raise_on_unexpected_status):
         self.base_url = auth_state.config().base_url
         self.timeout = 30.0
         self.cookies = {}
         self.headers = {}
         self.verify_ssl = True
-        self.raise_on_unexpected_status = True
+        self.raise_on_unexpected_status = raise_on_unexpected_status
         self.auth_state = auth_state
         self.follow_redirects = False
 
@@ -26,6 +27,8 @@ class PriceloopClient(AuthenticatedClient):
         return {"Authorization": auth_header_value, **self.headers}
 
     @staticmethod
-    def with_credentials(username: str, password: str, host: str = default_host) -> "PriceloopClient":
+    def with_credentials(
+        username: str, password: str, host: str = default_host, raise_on_unexpected_status=True
+    ) -> "PriceloopClient":
         auth_state = AuthState(username, password, host)
-        return PriceloopClient(auth_state)
+        return PriceloopClient(auth_state, raise_on_unexpected_status)

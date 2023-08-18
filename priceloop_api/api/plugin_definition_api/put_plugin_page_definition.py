c07f94b7ab209d05e3d5d4eee61b2e230b1b5a55
priceloop <ogopogo@priceloop.ai>
2023-08-18 13:35:37 +0000

    Generate api project for v0.154.0

--- priceloop_api/api/plugin_definition_api/put_plugin_page_definition.py
+++ priceloop_api/api/plugin_definition_api/put_plugin_page_definition.py
@@ -129,6 +129,7 @@ def sync_detailed(
         number: Icon,
         plus: Icon,
         row: Icon,
+        save: Icon,
         settings: Icon,
         sheet: Icon,
         shuffle: Icon,
@@ -230,6 +231,11 @@ def sync_detailed(
     type ChartOptions = { xAnnotations: ChartAnnotation[]?, yAnnotations: ChartAnnotation[]?,
     additionalApexOptions: Object? }
 
+    type PluginName = String
+
+    type PageId = String(PageId for usage with eta-functions like include, includeAsync or layout:
+    https://eta.js.org/docs/intro/template-syntax#partials-and-layouts)
+
     type TableRow = { values: String?[], byName: (column: ColumnName) => String? }
 
     type TableData = { rows: TableRow[] }
@@ -246,6 +252,7 @@ def sync_detailed(
         chart: (query: TableQuery, chartType: ChartType, x: ChartXAxis, y: ChartYAxis[], options:
     ChartOptions?) => UIBlock,
         chartCustom: (apexOptions: Object) => UIBlock,
+        pageId: (path: ViewPagePath, pluginName: PluginName?) => PageId,
         load: async (query: TableQuery) => TableData,
         loadOnce: async (query: TableQuery) => TableData
     }
@@ -379,6 +386,7 @@ async def asyncio_detailed(
         number: Icon,
         plus: Icon,
         row: Icon,
+        save: Icon,
         settings: Icon,
         sheet: Icon,
         shuffle: Icon,
@@ -480,6 +488,11 @@ async def asyncio_detailed(
     type ChartOptions = { xAnnotations: ChartAnnotation[]?, yAnnotations: ChartAnnotation[]?,
     additionalApexOptions: Object? }
 
+    type PluginName = String
+
+    type PageId = String(PageId for usage with eta-functions like include, includeAsync or layout:
+    https://eta.js.org/docs/intro/template-syntax#partials-and-layouts)
+
     type TableRow = { values: String?[], byName: (column: ColumnName) => String? }
 
     type TableData = { rows: TableRow[] }
@@ -496,6 +509,7 @@ async def asyncio_detailed(
         chart: (query: TableQuery, chartType: ChartType, x: ChartXAxis, y: ChartYAxis[], options:
     ChartOptions?) => UIBlock,
         chartCustom: (apexOptions: Object) => UIBlock,
+        pageId: (path: ViewPagePath, pluginName: PluginName?) => PageId,
         load: async (query: TableQuery) => TableData,
         loadOnce: async (query: TableQuery) => TableData
     }

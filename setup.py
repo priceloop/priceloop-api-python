e99dda194a62f3dd1d90f50ee05cda6cd7d0e4d9
priceloop <ogopogo@priceloop.ai>
2023-12-20 21:06:19 +0000

    Generate api project for v0.210.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ REQUIRES = [
 
 setup(
     name="priceloop-api",
-    version="0.210.5",
+    version="0.210.6",
     description="A client library for accessing Priceloop API",
     author="Priceloop",
     author_email="hello@priceloop.ai",
